a0a53c69f1ea254812df0ee949b9c4cad2b519bc
fatemeh137070 <fayaghoubi1370@gmail.com>
2025-02-18 01:04:10 +0330

    sing

--- src/main/java/water/WaterCooler.java
+++ src/main/java/water/WaterCooler.java
@@ -1,19 +1,31 @@
 package water;
 
-public class WaterCooler {
+import java.io.Serializable;
 
-    private static WaterCooler instance;
+public class WaterCooler implements Serializable {
+    private static volatile WaterCooler instance;
 
 
     private WaterCooler() {
-
+        if (instance != null) {
+            throw new IllegalStateException("Instance already created!");
+        }
     }
+
+
     public static WaterCooler getInstance() {
         if (instance == null) {
-
-            instance = new WaterCooler();
+            synchronized (WaterCooler.class) {
+                if (instance == null) {
+                    instance = new WaterCooler();
+                }
+            }
         }
         return instance;
+    }
+
+    protected Object readResolve() {
+        return getInstance();
     }
 
     public void drinkWater() {
